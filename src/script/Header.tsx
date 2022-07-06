465fa5dea0aa7f4da77fcaa9147e910d34f0b45b
Kamil Jakóbczak <k_j1@vp.pl>
2022-07-06 18:01:11 +0200

    initial setup

---
+++ src/script/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header className='App-header'>
+      <h1>Jamar's Book Collection</h1>
+      <nav>
+        <ul>
+          <li>Collection</li>
+          <li>New book</li>
+          <li>New author</li>
+          <li>New publisher</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
